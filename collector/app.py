c9dd70c7a2a19ccd21e8f4595c85cb1d6823ab62
sallyman128 <salmaan.arif.ali@gmail.com>
2023-11-07 18:23:05 -0500

    setup a test to confirm service

--- collector/app.py
+++ collector/app.py
@@ -3,10 +3,11 @@ from models import db
 from flask_migrate import Migrate
 from fetch_data import add_daily_data_from_codewars
 from sqlalchemy import inspect
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///codewars'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -18,3 +19,8 @@ def does_table_exist():
 with app.app_context():
     # Fetch daily data from API
     if does_table_exist(): add_daily_data_from_codewars()
+
+## NOTE just for testing the service... delete in the end...
+@app.route("/") 
+def index(): 
+    return "Collector is working"
