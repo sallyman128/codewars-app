dcda97e81711bf476bc229ab96063a5b2fe57912
sallyman128 <salmaan.arif.ali@gmail.com>
2023-11-07 18:41:42 -0500

    refactor db to check if exists before creating with sql alchemy

--- collector/app.py
+++ collector/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 from models import db
 from flask_migrate import Migrate
 from fetch_data import add_daily_data_from_codewars
-from sqlalchemy import inspect
+from sqlalchemy import inspect, create_engine
+from sqlalchemy_utils import database_exists, create_database
 import os
 
+engine = create_engine(os.environ.get('DATABASE_URL'))
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
