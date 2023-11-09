60abf0c9cbe4238050c3378623336438398a6742
sallyman128 <salmaan.arif.ali@gmail.com>
2023-11-09 13:13:20 -0500

    setup to UI to display fetched data

--- display/src/App.js
+++ display/src/App.js
@@ -1,8 +1,46 @@
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [analytics, setAnalytics] = useState();
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAnalytics({
+      data: [
+        { language: "ruby", color: "red", rank: 12 },
+        { language: "java", color: "green", rank: 3 },
+      ],
+    });
+  };
+
   return (
     <div className="App">
-      
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Enter Codewars Username: </label>
+          <input type="text" onChange={updateUsername} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div>
+        {analytics?.data ? (
+          <ul>
+            {analytics.data.map((item, index) => (
+              <li key={index}>
+                <strong>Language:</strong> {item.language},{" "}
+                <strong>Color:</strong> {item.color}, <strong>Rank:</strong>{" "}
+                {item.rank}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
     </div>
   );
 }
