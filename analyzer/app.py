a60ccf386a8d8c0f8cbcfeb4a0e4512cf750c92c
sallyman128 <salmaan.arif.ali@gmail.com>
2023-11-08 21:06:15 -0500

    add rabbitmq to docker

--- analyzer/app.py
+++ analyzer/app.py
@@ -1,9 +1,25 @@
-#!/usr/bin/env python3
-
 from flask import Flask
+import pika
 
 app = Flask(__name__)
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
 
-@app.route("/")
-def main():
-    return 'Analyzer is working'
+channel.queue_declare(queue='example-queue')
+
+@app.route('/')
+def produce_message():
+    channel.basic_publish(exchange='', routing_key='example-queue', body='Hello, RabbitMQ!')
+    return 'Message sent to RabbitMQ queue.'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def main():
+#     return 'Analyzer is working'
--- display/app.py
+++ display/app.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python3
-
+# consumer/app.py
 from flask import Flask
+import pika
 
 app = Flask(__name__)
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
 
-@app.route("/")
-def main():
-    return 'Display is working'
+channel.queue_declare(queue='example-queue')
+
+@app.route('/')
+def consume_message():
+    method_frame, header_frame, body = channel.basic_get(queue='example-queue')
+    if method_frame:
+        return f"Received message: {body}"
+    else:
+        return "No messages in the queue."
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def main():
+#     return 'Display is working'
